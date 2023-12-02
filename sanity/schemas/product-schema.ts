8801f89d1276e0baccfb22f26e665327a602080b
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-12-02 10:01:05 +0100

    update header, add product-schema

--- sanity/schemas/product-schema.ts
+++ sanity/schemas/product-schema.ts
@@ -1,1 +1,73 @@
-export const product = {}
+import { type } from "os";
+import { defineField, defineType } from "sanity";
+
+export const product = defineType({
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+        defineField(
+            {
+                name: 'title',
+                title: 'Title',
+                type: 'string'
+            }),
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'name'
+            }
+        },
+        {
+            name: 'images',
+            title: 'Images',
+            type: 'array',
+            of: [{ type: 'image' }]
+        },
+        {
+            name: 'categories',
+            title: 'Categories',
+            type: 'array',
+            of: [{ type: 'string' }]
+        },
+        {
+            name: 'sizes',
+            title: 'Sizes',
+            type: 'array',
+            of: [{ type: 'string' }]
+        },
+        {
+            name: 'colors',
+            title: 'Colors',
+            type: 'array',
+            of: [{ type: 'string' }]
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'string'
+        },
+        {
+            name: 'sku',
+            title: 'Sku',
+            type: 'string'
+        },
+        {
+            name: 'currency',
+            title: 'Currency',
+            type: 'string'
+        },
+        {
+            name: 'price',
+            title: 'Price',
+            type: 'number'
+        }
+
+
+    ]
+
+
+
+})
