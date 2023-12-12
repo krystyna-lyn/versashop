e5311bdaf99f7f8e3f0d4dc577374260bc811930
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-12-12 13:29:06 +0000

    update filters

--- app/page.tsx
+++ app/page.tsx
@@ -15,14 +15,15 @@ interface Props {
     price?: string,
     color?: string,
     category?: string,
-    size?: string
+    size?: string,
+    search?: string
   }
 
 }
 
 export default async function Page({ searchParams }: Props) {
 
-  const { date = 'desc', price, color, category, size } = searchParams
+  const { date = 'desc', price, color, category, size, search } = searchParams
 
   const priceOrder = price
     ? `| order(price ${price})` : ""
@@ -34,9 +35,10 @@ export default async function Page({ searchParams }: Props) {
 
   const productFilter = `_type == 'product'`;
   const colorFilter = color ? `&& '${color}' in colors` : '';
-  const categoryFilter = color ? `&& '${category}' in categories` : '';
-  const sizeFilter = color ? `&& '${size}' in sizes` : '';
-  const filter = `*[${productFilter} ${colorFilter}${categoryFilter}${sizeFilter}]`
+  const categoryFilter = category ? `&& '${category}' in categories` : '';
+  const sizeFilter = size ? `&& '${size}' in sizes` : '';
+  const searchFilter = search ? `&& name match '${search}'` : '';
+  const filter = `*[${productFilter} ${colorFilter}${categoryFilter}${sizeFilter}${searchFilter}]`
 
 
   const products = await client.fetch<SanityProduct[]>(
@@ -50,8 +52,8 @@ export default async function Page({ searchParams }: Props) {
     price,
     description,
     'slug': slug.current
-  }`
-  )
+  }`)
+
   console.log(products)
 
   return (
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -3,15 +3,28 @@
 import Link from "next/link"
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { Edit, ShoppingBag } from "lucide-react"
-import { useShoppingCart } from "use-shopping-cart"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 
+
 export function SiteHeader() {
-  const pathname = usePathname()
+
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const defaultSearchQuery = searchParams.get('search') ?? "";
+
+  function onSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
+
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const searchQuery = formData.get('search');
+    router.replace(`/?search=${searchQuery}`);
+
+  }
 
   if (pathname.startsWith('/studio')) return null
 
@@ -19,14 +32,18 @@ export function SiteHeader() {
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between space-x-4 px-6 sm:space-x-0">
         <MainNav />
-        <form className="hidden items-center lg:inline-flex">
+        <form
+          onSubmit={onSubmit}
+          className="hidden items-center lg:inline-flex">
           <Input
+
             id="search"
             name="search"
             type="search"
             autoComplete="off"
             placeholder="Search products..."
             className="h-9 lg:w-[300px]"
+            defaultValue={defaultSearchQuery}
           />
         </form>
         <div className="flex items-center space-x-1">
