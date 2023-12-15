bae86391ddb4e459b907aa8cee0796f1fafc212a
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-12-15 21:24:04 +0000

    add product images

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -36,9 +36,9 @@ export default async function Page({ params }: Props) {
   return (
     <main className="mx-auto max-w-5xl sm:px-6 sm:pt-16 lg:px-8">
       <div className="mx-auto max-w-2xl lg:max-w-none">
-
+          
         <div className="pb-20 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-12">
-
+          <ProductGallery product={product}/>
           <ProductInfo product={product} />
 
         </div>
