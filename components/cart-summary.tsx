c7f87006af7ca325fa712340213830d0ccc4108d
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-12-19 14:19:37 +0000

    Update cart-summary.tsx

--- components/cart-summary.tsx
+++ components/cart-summary.tsx
@@ -7,7 +7,18 @@ import { formatCurrencyString, useShoppingCart } from "use-shopping-cart"
 import { Button } from "@/components/ui/button"
 
 export function CartSummary() {
-  function onCheckout() {}
+
+  const { formattedTotalPrice, totalPrice, cartDetails, cartCount } = useShoppingCart()
+
+  const shippingAmount = cartCount! > 0 ? 500 : 0
+  const totalAmount = totalPrice! + shippingAmount
+
+  const [isLoading, setLoading] = useState(false)
+  const isDisabled = isLoading || cartCount! === 0
+
+  function onCheckout() {
+
+  }
 
   return (
     <section
@@ -21,24 +32,35 @@ export function CartSummary() {
       <dl className="mt-6 space-y-4">
         <div className="flex items-center justify-between">
           <dt className="text-sm">Subtotal</dt>
-          <dd className="text-sm font-medium">Subtotal Amount</dd>
+          <dd className="text-sm font-medium">{formattedTotalPrice}</dd>
         </div>
         <div className="flex items-center justify-between border-t border-gray-200 pt-4 dark:border-gray-600">
           <dt className="flex items-center text-sm">
             <span>Shipping estimate</span>
           </dt>
-          <dd className="text-sm font-medium">Shipping Amount</dd>
+          <dd className="text-sm font-medium">
+            {formatCurrencyString({
+              value: shippingAmount,
+              currency: "EUR"
+            })}</dd>
         </div>
         <div className="flex items-center justify-between border-t border-gray-200 pt-4 dark:border-gray-600">
-          <dt className="text-base font-medium">Order total</dt>
+          <dt className="text-base font-medium">
+            {formatCurrencyString({
+              value: totalAmount,
+              currency: "EUR"
+            })}
+          </dt>
           <dd className="text-base font-medium">Order Amount</dd>
         </div>
       </dl>
 
       <div className="mt-6">
-        <Button className="w-full">
-          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-          Loading...
+        <Button onClick={onCheckout} className="w-full" disabled={isDisabled}>
+          {isLoading &&
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          }
+          {isLoading ? 'Loading...' : 'Checkout'}
         </Button>
       </div>
     </section>
